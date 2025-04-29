66a3625ded56f2fd3d18fa2a298d2398f8b4387f
Ana Ludimilla dos Santos Pereira <analudimilla4@gmail.com>
2025-04-29 17:30:56 -0300

    Update 05_indices.sql

--- loja_virtual/05_indices.sql
+++ loja_virtual/05_indices.sql
@@ -1,2 +1,5 @@
-CREATE INDEX idx_email ON clientes(email); 
-CREATE INDEX idx_id_cliente ON vendas(id_cliente); 
+-- Índice para melhorar busca por e-mail
+CREATE INDEX idx_email ON clientes(email);
+
+-- Índice para acelerar JOINs com id_cliente
+CREATE INDEX idx_id_cliente ON vendas(id_cliente);
